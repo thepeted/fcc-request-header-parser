a63f7a5251fcd71dcf2868d8e9c8b59d3b58d8b5
Peter Daily <peter.daily@yahoo.co.uk>
2017-04-29 11:18:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# FCC API Projects: Request Header Parser
+## User Story:
+1. I can get the IP address, language and operating system for my browser.
+
+## Example Usage:
+`https://thepeted-request-header-parser.herokuapp.com/whoami/`
+
+## Example Response
+`{
+  "ipaddress": "86.156.89.175",
+  "language": "en-GB",
+  "software": "Macintosh; Intel Mac OS X 10_12_4"
+}`
---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+const http = require('http')
+const getIP = require('ipware')().get_ip
+
+const server = http.createServer((req, res) => {
+  if (/^\/whoami\/?$/.test(req.url)) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({
+      ipaddress: getIP(req).clientIp,
+      language: req.headers['accept-language'].split(',')[0],
+      software: req.headers['user-agent'].match(/\((.*?)\)/)[1]
+    }))  
+  } else {
+    res.writeHead(404)
+    res.end()
+  }
+})
+
+const port = process.env.PORT || 3000
+server.listen(port)
